f687be0df120d604dc2bdb16f517e32c7a82516f
Mikko Nummelin <mnummeli@gmail.com>
2019-10-24 22:38:37 +0300

    A simple ToDo list almost ready.

---
+++ app.js
@@ -0,0 +1,51 @@
+function app() {
+    const todoItems = [], doneItems = [];
+    const newItemForm = document.getElementById('newItem');
+    newItemForm.onsubmit = function (event) {
+        // Ei ladata sivua uudestaan kun lomake lähetetään
+        event.preventDefault();
+        const itemTextInput = event.target.querySelector('input');
+        const itemText = itemTextInput.value;
+        todoItems.push(itemText);
+        updateTodoItems();
+        itemTextInput.value = '';
+    };
+    
+    const todoItemsForm = document.getElementById('todoItems');
+    todoItemsForm.onsubmit = function (event) {
+        // Ei ladata sivua uudestaan kun lomake lähetetään
+        event.preventDefault();
+        const itemTextSelect = event.target.querySelector('select');
+        const removedItem = parseInt(itemTextSelect.value.split('_')[1]);
+        doneItems.push(todoItems[removedItem]);
+        todoItems.splice(removedItem, 1);
+        updateTodoItems();
+        updateDoneItems();
+    };
+
+    function updateTodoItems() {
+        const todoItemList = document.getElementById('todoItemList');
+        todoItemList.innerHTML = '';
+        for (let i = 0; i < todoItems.length; i++) {
+            let option = document.createElement('option');
+            let textNode = document.createTextNode(todoItems[i]);
+            option.appendChild(textNode);
+            option.value = `v_${i}`;
+            todoItemList.appendChild(option);
+        }
+    }
+    
+    function updateDoneItems() {
+        const doneItemsList = document.querySelector('#doneItems ul');
+        doneItemsList.innerHTML = '';
+        for (let i = 0; i < doneItems.length; i++) {
+            let option = document.createElement('li');
+            let textNode = document.createTextNode(doneItems[i]);
+            option.appendChild(textNode);
+            doneItemsList.appendChild(option);
+        }
+    }
+}
+
+// Suoritetaan Javascript-koodi vasta kun koko ikkuna on latautunut
+window.onload = app;
